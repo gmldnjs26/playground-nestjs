4118815ff9f5de9959dd7c5ffaa512387c549f13
gmldnjs26 <imleesky265@gmail.com>
2025-03-23 17:24:40 +0900

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    PostModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'test',
+      password: process.env.DB_PASSWORD || 'test',
+      database: process.env.DB_DATABASE || 'playground-nestjs',
+      autoLoadEntities: true, // 엔티티 파일(*.entity.ts) 자동 로드
+      synchronize: true, // 엔티티 파일 변경 시 테이블 자동 동기화
+      logging: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/post/post.entity.ts
@@ -0,0 +1,33 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Entity } from 'typeorm';
+
+@Entity({
+  name: 'posts',
+})
+export class Post {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+  })
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    type: 'text',
+    nullable: false,
+  })
+  content: string;
+
+  @Column({
+    type: 'int',
+    name: 'author_id',
+    nullable: false,
+  })
+  authorId: number;
+}
